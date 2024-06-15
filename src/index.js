50829344d811c13c4aac816120900b45154c04b0
Alapan <alapandas333@gmail.com>
2024-06-16 01:22:02 +0530

    Minor Fix

--- src/index.js
+++ src/index.js
@@ -2,20 +2,18 @@ import React from 'react';
 import * as ReactDOM from "react-dom/client";
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from './AuthContext';
+import { Analytics } from "@vercel/analytics/react"
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthProvider>
-      <App/>
+      <App />
     </AuthProvider>
+    <Analytics />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log());
